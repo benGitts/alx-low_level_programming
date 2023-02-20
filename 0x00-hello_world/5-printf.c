3173d1554fa4381126c227579d59d20b50b78328
Benson Gitau <benxot@gmail.com>
2023-02-20 14:52:43 -0800

    6-size.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line
+ * Description - using printf function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
