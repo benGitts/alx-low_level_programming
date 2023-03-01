fa81f48fa7fa4d590d1f03c698160b8802810c6e
Benson Gitau <benxot@gmail.com>
2023-02-28 18:54:52 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _puts - prints a string
+* @str: single character type
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+	_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
+}
